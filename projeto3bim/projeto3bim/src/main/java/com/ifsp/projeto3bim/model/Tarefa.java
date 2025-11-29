76041a8c013ecd133cf90ef0d0bd08a97c6e7ff6
gi <135059916+giovannalimang@users.noreply.github.com>
2025-11-29 01:00:42 -0300

    Add files via upload
    Atualização do trabalho e implementaão do Banco de Dados

---
+++ projeto3bim/projeto3bim/src/main/java/com/ifsp/projeto3bim/model/Tarefa.java
@@ -0,0 +1,82 @@
+package com.ifsp.projeto3bim.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Entity
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String data; 
+    private String status; 
+
+    public Tarefa() {
+    }
+
+    public Tarefa(String nome, String data, String status) {
+        this.nome = nome;
+        this.data = data;
+        this.status = status;
+    }
+
+    public void toggleStatus() {
+        if ("Completed".equalsIgnoreCase(this.status)) {
+            this.status = "Pending";
+        } else if ("Pending".equalsIgnoreCase(this.status)) {
+            this.status = "Completed";
+        } else if ("Processing".equalsIgnoreCase(this.status)) {
+            this.status = "Completed"; 
+        } else {
+            this.status = "Pending";
+        }
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    public LocalDate getDataAsLocalDate() {
+        try {
+            return LocalDate.parse(this.data, DateTimeFormatter.ISO_DATE);
+        } catch (Exception e) {
+          
+            return null; 
+        }
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
