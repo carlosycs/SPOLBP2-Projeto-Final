0bb44ae4d959f441f2a1feb4b697a8d5e2dc94e4
gi <135059916+giovannalimang@users.noreply.github.com>
2025-11-29 01:04:24 -0300

    Delete projeto3bim/projeto3bim directory

--- projeto3bim/projeto3bim/src/main/java/com/ifsp/projeto3bim/repository/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ifsp.projeto3bim.repository;
-
-import com.ifsp.projeto3bim.model.Tarefa;
-import org.springframework.data.repository.CrudRepository; 
-import java.util.List;
-
-public interface TarefaRepository extends CrudRepository<Tarefa, Long> {
-    List<Tarefa> findByStatus(String status);
-}
